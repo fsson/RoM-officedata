26387f3a62f26e6b3db40e07dfd450f446dad36c
fsson <128136044+fsson@users.noreply.github.com>
2025-02-20 21:37:04 +0100

    Create module

--- scripts/fetcher.py
+++ scripts/fetcher.py
@@ -1,48 +1,41 @@
-import subprocess
-import sys
-
-def install(package):
-  try:
-    __import__(package)
-  except ImportError:
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
-
-packages = ['requests', 'pandas', 'json', 'datetime']
-
-for p in packages:
-    install(p)
-
 import requests
-import pandas as pd
 import json
 from datetime import date
 
+# Function for fetching JSON data from AF
 def fetch_data(page):
     response = requests.get(f"https://arbetsformedlingen.se/rest/rusta-och-matcha-2/sokleverantor/v2/leverantorer?sida={page}&tjanstekoder=A015")
+    # Fetch data if request succeeded
     if response.status_code == 200:
         data = response.json()
+        # Return None at last page
         if data.get("total_count", 0) == 0:
             return None  
         suppliers = data.get("leverantorer", [])
         if not suppliers:
             return None
         return data
+    # Return error if request failed
     print(f"Request failed: status code {response.status_code}")
 
+# Function for applying fetch_data() to all pages and returning the combined result
 def fetch_iterator():
     all_data = []
     page = 1
     while True:
-        print(f"Fetching from page {page}")
+        print(f"Fetching from page {page}", end="\r")
         data = fetch_data(page)
+        # Break when fetch_data() returns None (last page)
         if data is None:
             return all_data
             break
         all_data.append(data)
         page += 1
 
-def create_file():
+# Function for creating JSON file out of combined JSON data
+def create_json():
     data = fetch_iterator()
+    # Write to file if data is returned
     if data:
         today = date.today()
         with open(f"data/raw/office_data_{today}.json", "w", encoding="utf-8") as f:
@@ -50,5 +43,3 @@ def create_file():
             print("File successfully created.")
     else:
         print(f"Something went wrong! File creation aborted.")
-
-create_file()
