2c11b1c6f5bc26edca94a710ac21f35903358070
fsson <128136044+fsson@users.noreply.github.com>
2025-03-01 16:47:49 +0100

    Add automated github pull/push

--- run.sh
+++ run.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+# Pull changes from GitHub
+echo "Pulling latest changes from GitHub..."
+git pull
+
 # Check python/pip version
 PIP_CMD=$(command -v pip || command -v pip3)
 PYTHON_CMD=$(command -v python || command -v python3)
 
 # Install necessary packages
-echo "Installing packages, if nessecary..."
+echo "Installing packages if nessecary..."
 $PIP_CMD install -r requirements.txt &> /dev/null
 
 # Run main script
 $PYTHON_CMD main.py
+
+# Push changes to GitHub
+echo "Pushing changes GitHub..."
+git commit -m "Automatic update $(date -I)"
+git push
